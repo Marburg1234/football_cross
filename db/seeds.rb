35fd9f52717dadc724ae19144995a40c35a0412c
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-11-16 04:44:15 +0000

    ユーザー作成x3 db/seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ユーザーを登録1
+unless User.find_by(nickname: '11')
+  User.create!(nickname: '11') do |user|
+      user.email = '1@yahoo.co.jp'
+      user.password = '111111'
+  end
+end
+
+# ユーザーを登録2
+unless User.find_by(nickname: '22')
+  User.create!(nickname: '22') do |user|
+      user.email = '2@yahoo.co.jp'
+      user.password = '222222'
+  end
+end
+
+# ユーザーを登録3
+unless User.find_by(nickname: '33')
+  User.create!(nickname: '33') do |user|
+      user.email = '3@yahoo.co.jp'
+      user.password = '333333'
+  end
+end
+
+
+
